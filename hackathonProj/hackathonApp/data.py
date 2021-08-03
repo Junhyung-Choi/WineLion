f68e57e44bfe8accbb65a376aab3e1233298c516
Junhyung-Choi <junhyung9571@naver.com>
2021-08-03 09:57:45 +0900

    UPDATE DB && FIX Foods location && UPDATE data insert functions to comment

--- hackathonProj/hackathonApp/data.py
+++ hackathonProj/hackathonApp/data.py
@@ -1,3 +1,12 @@
+# ===================================================
+# =              코드 수정 전에 주의사항!           =
+# =              2021-08-03 09:52 기준              =
+# =              CSV 기준으로 DB입력이 끝나서       =
+# =              db.sqlite3 파일을 지우지 않는한    =
+# =              아래 함수들을 사용하지 말 것!      =
+# =                  - Junhyung-Choi -              =
+# ===================================================
+
 import csv
 import os
 import sys
@@ -35,7 +44,6 @@ def insert_wine():
     print("와인 성공!")
 
 def insert_food():
-    
     CSV_PATH = os.path.join(BASE_DIR,"../data/fooddata.csv")
     with open(CSV_PATH, newline='') as csvfile:
         data_reader = csv.DictReader(csvfile)
@@ -67,6 +75,20 @@ def insert_review():
             )
             
     print("리뷰 성공!")
+
+def insert_wine2food():
+    
+    CSV_PATH = os.path.join(BASE_DIR,"../data/wine2fooddata.csv")
+    with open(CSV_PATH, newline='') as csvfile:
+        data_reader = csv.DictReader(csvfile)
+        for row in data_reader:
+            print(row)
+            wine = Wine.objects.get(id=int(row['wine_id']))
+            #음식 데이터셋 3번 넣고 지우니까 id 71 * 3개 밀려서 213 더해놨음 db.sqlite3 파일 삭제 후 다시 진행시 213 지울 것
+            food = Food.objects.get(id=(int(row['food_id'])+213))
+            food.wines.add(wine)
+            food.save()
+    print("음식와인연결 성공!")
 
 # insert_wine()
 # insert_food()
--- hackathonProj/hackathonApp/views.py
+++ hackathonProj/hackathonApp/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def main(request):
-    # context = {"wine_data" : insert_wine()}
-    return render(request,'practice.html')
+    context = {
+        "wine":Wine.objects.get(id = 1),
+        "food":Food.objects.get_queryset()[0].id
+    }
+    return render(request,'practice.html',context)
 
+# ===================================================
+# =              코드 수정 전에 주의사항!           =
+# =              2021-08-03 09:52 기준              =
+# =              CSV 기준으로 DB입력이 끝나서       =
+# =              db.sqlite3 파일을 지우지 않는한    =
+# =              insert_data를 사용하지 말 것!      =
+# =                  - Junhyung-Choi -              =
+# ===================================================
 def insert_data(request):
     #insert_wine()
     #insert_food()
-    insert_review()
+    #insert_review()
+    #insert_wine2food()
     return redirect('main')
 
 
