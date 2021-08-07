c6c0855299360804e0df18c4275be2861d273686
psm <psm401@hanyang.ac.kr>
2021-08-07 15:24:22 +0900

    .

--- hackathonProj/hackathonApp/static/js/script.js
+++ hackathonProj/hackathonApp/static/js/script.js
@@ -49,3 +49,26 @@ function showSlides(n) {
   slides[n].style.display = "block";
   dots[n].className += " active";
 }
+
+// 와인추천
+
+
+new Swiper('.swiper_slide', {
+
+	slidesPerView : 5, // 동시에 보여줄 슬라이드 갯수
+	spaceBetween : 10, 
+	slidesPerGroup : 5, 
+
+	loopFillGroupWithBlank : true,
+
+	loop : true, // 무한 반복
+
+	pagination : { // 페이징
+		el : '.swiper-slide',
+		clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+	},
+	navigation : { // 네비게이션
+		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+	},
+});
