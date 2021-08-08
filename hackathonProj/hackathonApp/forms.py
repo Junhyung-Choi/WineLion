933a688a1f26c78650c678a9614a108ea60eb68f
Junhyung-Choi <junhyung9571@naver.com>
2021-08-08 11:02:46 +0900

    UPDATE Singup

--- hackathonProj/hackathonApp/forms.py
+++ hackathonProj/hackathonApp/forms.py
@@ -8,6 +8,8 @@ from .models import CustomUser
 
 #views.py에서 RegisterForm을 불러와야 이제 유저 생성 form을 갖다 쓸 수 있다.
 class RegisterForm(UserCreationForm):
+    birth = forms.DateField(required=True)
+    gender = forms.ChoiceField(choices=[("M","남성"),("F","여성")], required=True)
     class Meta:
         model = CustomUser
         fields = ['username', 'password1','password2', 'birth', 'gender']
